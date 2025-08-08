eb103e20f151ceb8cb9028ec79181fdaaaa8bbd4
yeab <yeab.dev@gmail.com>
2025-08-08 19:49:09 +0300

    feat: add data layer

---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:yegna_gebeya/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final FirebaseAuth firebaseAuth;
+  const AuthRepositoryImpl({required this.firebaseAuth});
+  @override
+  Future<UserCredential> signUpWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    final credintial = await firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credintial;
+  }
+
+  @override
+  Future<UserCredential> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+}
