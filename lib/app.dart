8fd47d1219bb38691a9d3a9610737b93fd642af3
yeab <yeab.dev@gmail.com>
2025-08-11 13:14:29 +0300

    merge: edit sizes and color to use predefined pros

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yegna_gebeya/core/locator.dart';
 import 'package:yegna_gebeya/features/auth/presentation/cubit/sign_up_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:yegna_gebeya/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:yegna_gebeya/features/landing/presentation/pages/landing_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: MultiBlocProvider(
         providers: [BlocProvider(create: (context) => getIt<SignUpCubit>())],
-        child: SignUpPage(),
+        child: LandingPage(),
       ),
     );
   }
