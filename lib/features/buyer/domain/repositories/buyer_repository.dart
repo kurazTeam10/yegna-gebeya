4d8180c8a173236dd9768471422f68cbd21380c4
Abdurezakkemal <abdrezakkemal@gmail.com>
2025-08-11 12:41:55 +0200

    feat: add domain layer

--- lib/features/buyer/domain/repositories/buyer_repository.dart
+++ lib/features/buyer/domain/repositories/buyer_repository.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:yegna_gebeya/core/models/product.dart';
 import 'package:yegna_gebeya/core/models/cart.dart';
+import 'package:yegna_gebeya/core/models/seller.dart';
 
 abstract class ProductRepository {
   Future<List<Product>> getProducts();
   Future<Product> getProductById(String productId);
+  Future<Seller> getSeller(DocumentReference sellerRef);
 }
 
 abstract class CartRepository {
