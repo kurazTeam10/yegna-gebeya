1f30c879bb9d2d4616100f0f7f12245875ed66d0
HyoketsuSenpai <Youseftela@gmail.com>
2025-08-13 15:36:04 +0300

    feat(di): register FirebaseFirestore, BuyerRepositoryImpl, and CartBloc

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:yegna_gebeya/features/auth/data/repositories/auth_repository.dart';
 import 'package:yegna_gebeya/features/auth/domain/repositories/auth_repository.dart';
 import 'package:yegna_gebeya/features/auth/presentation/cubit/sign_up_cubit.dart';
+import 'package:yegna_gebeya/features/buyer/data/repositories/buyer_repository_impl.dart';
+import 'package:yegna_gebeya/features/buyer/domain/repositories/buyer_repository.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/bloc/cart_bloc.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -16,4 +20,11 @@ void setupLocator() {
   getIt.registerFactory<SignUpCubit>(
     () => SignUpCubit(authRepo: getIt<AuthRepository>()),
   );
+
+  getIt.registerLazySingleton<FirebaseFirestore>(() => FirebaseFirestore.instance);
+
+  getIt.registerLazySingleton<BuyerRepository>(() => BuyerRepositoryImpl(firestore: getIt<FirebaseFirestore>()));
+
+  getIt.registerFactory<CartBloc>(() => CartBloc(repository: getIt<BuyerRepository>()));
+
 }
