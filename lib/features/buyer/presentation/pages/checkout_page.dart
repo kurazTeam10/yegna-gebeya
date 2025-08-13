1235817ca2454e7a5039119383d7ebb3b95fba41
HyoketsuSenpai <Youseftela@gmail.com>
2025-08-13 15:37:08 +0300

    feat(ui): add CheckoutPage and CartIconWidget

---
+++ lib/features/buyer/presentation/pages/checkout_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yegna_gebeya/core/locator.dart';
+import 'package:yegna_gebeya/features/buyer/data/models/product.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/bloc/cart_bloc.dart';
+
+//TODO: add proper id from an auth cubit/bloc
+
+class CheckoutPage extends StatefulWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+}
+
+class _CheckoutPageState extends State<CheckoutPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    final cartBloc = getIt<CartBloc>();
+
+    if (cartBloc.state is! CartLoaded && cartBloc.state is! CartLoading) {
+      cartBloc.add(GetCartEvent(id: '1'));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cart'), centerTitle: true),
+      body: BlocConsumer<CartBloc, CartState>(
+        listener: (context, state) {
+          if (state is CartError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          if (state is CartLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (state is CartLoaded) {
+            final products = state.products;
+            return ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                final quantity = products
+                    .where((p) => p.id == product.id)
+                    .length;
+
+                return Row(
+                  children: [
+                    Image(image: NetworkImage(product.photoUrl)),
+                    Column(
+                      children: [Text(product.name), Text('${product.cost}')],
+                    ),
+                    Column(
+                      children: [
+                        IconButton(
+                          onPressed: () => getIt<CartBloc>().add(
+                            AddToCartEvent(id: '1', product: product),
+                          ),
+                          icon: Icon(Icons.arrow_upward),
+                        ),
+                        Text(quantity.toString()),
+                        IconButton(
+                          onPressed: () => getIt<CartBloc>().add(
+                            RemoveFromCartEvent(id: '1', product: product),
+                          ),
+                          icon: Icon(Icons.arrow_downward),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            );
+          }
+
+          return Center(child: Text('Cart is empty'));
+        },
+      ),
+    );
+  }
+}
---
+++ lib/features/buyer/presentation/widgets/cart_icon_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yegna_gebeya/core/locator.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/bloc/cart_bloc.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/pages/checkout_page.dart';
+
+//TODO: add proper id from an auth cubit/bloc
+
+class CartIconWidget extends StatefulWidget {
+  const CartIconWidget({super.key});
+
+  @override
+  State<CartIconWidget> createState() => _CartIconWidgetState();
+}
+
+class _CartIconWidgetState extends State<CartIconWidget> {
+  @override
+  void initState() {
+    super.initState();
+
+    final cartBloc = getIt<CartBloc>();
+
+    if (cartBloc.state is! CartLoaded && cartBloc.state is! CartLoading) {
+      cartBloc.add(GetCartEvent(id: '1'));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<CartBloc, CartState>(
+      listener: (context, state) {
+        if (state is CartError) {
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.message)));
+        }
+      },
+      child: BlocBuilder<CartBloc, CartState>(
+        builder: (context, state) {
+          int itemCount = 0;
+
+          if (state is CartLoaded) {
+            itemCount = state.products.length;
+          }
+
+          return Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Icon(Icons.shopping_cart, size: 28),
+              if (itemCount > 0)
+                Positioned(
+                  right: -4,
+                  top: -4,
+                  child: CircleAvatar(
+                    radius: 8,
+                    backgroundColor: Colors.red,
+                    child: Text(
+                      '$itemCount',
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
