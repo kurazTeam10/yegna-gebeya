fcb5e982669ec6a957edd43a9bd9b1ac7ec7b04e
HyoketsuSenpai <Youseftela@gmail.com>
2025-08-18 12:57:29 +0300

    refactor: make OrderState classes extend Equatable
    - Added props for OrderInitial, OrderLoading, OrderLoaded, and OrderError
    - Enables proper state comparison in Bloc

--- lib/features/buyer/presentation/bloc/order_bloc/order_state.dart
+++ lib/features/buyer/presentation/bloc/order_bloc/order_state.dart
@@ -1,20 +1,32 @@
 part of 'order_bloc.dart';
 
 @immutable
-sealed class OrderState {}
+sealed class OrderState extends Equatable {}
 
-final class OrderInitial extends OrderState {}
+final class OrderInitial extends OrderState {
+  @override
+  List<Object?> get props => [];
+}
 
-final class OrderLoading extends OrderState {}
+final class OrderLoading extends OrderState {
+  @override
+  List<Object?> get props => [];
+}
 
 final class OrderLoaded extends OrderState {
   final List<Order> orders;
 
   OrderLoaded({required this.orders});
+
+  @override
+  List<Object?> get props => [...orders];
 }
 
 final class OrderError extends OrderState {
   final String message;
 
   OrderError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
 }
