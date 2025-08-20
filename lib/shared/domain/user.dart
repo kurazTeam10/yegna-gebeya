874e174df94b2b21f2c0bdfaa3532286158562ae
yeab <yeab.dev@gmail.com>
2025-08-20 11:16:02 +0300

    feat: add a shared user model

---
+++ lib/shared/domain/user.dart
@@ -0,0 +1,22 @@
+class User {
+  final String userId;
+  final String email;
+  final String fullName;
+  final String imgUrl;
+
+  const User({
+    required this.userId,
+    required this.email,
+    required this.fullName,
+    required this.imgUrl,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'email': email,
+      'fullName': fullName,
+      'imgUrl': imgUrl,
+    };
+  }
+}
