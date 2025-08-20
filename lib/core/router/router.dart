309103d34d41f5138ccd5bc5102d9804c0d50a7e
Abdurezakkemal <abdrezakkemal@gmail.com>
2025-08-20 23:52:44 +0200

    fix: route to sellerList to test ui

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,10 +1,10 @@
 import 'package:go_router/go_router.dart';
 import 'package:yegna_gebeya/core/router/routes.dart';
-// import 'package:yegna_gebeya/features/auth/presentation/pages/sign_in_page.dart';
-// import 'package:yegna_gebeya/features/auth/presentation/pages/sign_up_page.dart';
+import 'package:yegna_gebeya/features/auth/presentation/pages/sign_in_page.dart';
+import 'package:yegna_gebeya/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:yegna_gebeya/features/buyer/presentation/pages/seller_list_page.dart';
 import 'package:yegna_gebeya/features/buyer/presentation/pages/seller_profile_page.dart';
-// import 'package:yegna_gebeya/features/landing/presentation/pages/landing_page.dart';
+import 'package:yegna_gebeya/features/landing/presentation/pages/landing_page.dart';
 
 final goRouter = GoRouter(
   routes: [
@@ -19,14 +19,13 @@ final goRouter = GoRouter(
         return SellerProfilePage(sellerId: sellerId);
       },
     ),
-    // Auth routes are commented out as requested
-    // GoRoute(
-    //     path: Routes.signUp, builder: (context, state) => const SignUpPage()),
-    // GoRoute(
-    //     path: Routes.signIn, builder: (context, state) => const SignInPage()),
-    // GoRoute(
-    //   path: Routes.landigPage,
-    //   builder: (context, state) => const LandingPage(),
-    // ),
+    GoRoute(
+        path: Routes.signUp, builder: (context, state) => const SignUpPage()),
+    GoRoute(
+        path: Routes.signIn, builder: (context, state) => const SignInPage()),
+    GoRoute(
+      path: Routes.landingPage,
+      builder: (context, state) => const LandingPage(),
+    ),
   ],
 );
--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,6 +1,6 @@
 class Routes {
-  static const String landingPage = '/';
-  static const String sellerList = '/sellers';
+  static const String landingPage = '/landing';
+  static const String sellerList = '/';
   static const String signUp = '/signUp';
   static const String signIn = '/signIn';
   static const String sellerProfile = '/sellerProfile/:sellerId';
