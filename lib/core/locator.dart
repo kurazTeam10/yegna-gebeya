eeef1834fe8f7ee62f37d15b9ea2202b94eed028
Abdurezakkemal <abdrezakkemal@gmail.com>
2025-08-21 02:49:18 +0200

    refactor: change folder strucure

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -6,8 +6,8 @@ import 'package:yegna_gebeya/features/auth/presentation/cubits/sign_in/sign_in_c
 import 'package:yegna_gebeya/features/auth/presentation/cubits/sign_up/sign_up_cubit.dart';
 import 'package:yegna_gebeya/features/buyer/data/repositories/buyer_repository_impl.dart';
 import 'package:yegna_gebeya/features/buyer/domain/repositories/buyer_repository.dart';
-import 'package:yegna_gebeya/features/buyer/presentation/bloc/seller_list_bloc.dart';
-import 'package:yegna_gebeya/features/buyer/presentation/bloc/seller_profile_bloc.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/bloc/sellerList/seller_list_bloc.dart';
+import 'package:yegna_gebeya/features/buyer/presentation/bloc/sellerProfile/seller_profile_bloc.dart';
 
 final GetIt getIt = GetIt.instance;
 
